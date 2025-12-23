8da6a874fa163d4124e5783a2c815b1a3010464c
Semerikov_Stanislav <stassemerikov@gmail.com>
2025-12-23 15:01:01 +0300

    feat(core): add Entry entity, input function and update write_data
    - Implemented Entry dataclass for structured data
    - Added add_entry() to create new entries
    - Updated write_data() to append entries via asdict()

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import json
 from pathlib import Path
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class Entry:
+    service: str
+    login: str
+    email: str
+    password: str
+
 
 # IDEA: добавить GUI через tkinter
 # IDEA: добавить поиск по service/email
 # IDEA: добавить SQLite хранение данных
 
 
+service_data = "service"
+login_data = "login"
+email_data = "email@mail.com"
+password_data = "password1"
 
-email_input_data = "QWERTY@gmai.com"  # input("Введите Email адрес - ")
-password_input_data = "QWErty12345"  # input("Введите пароль - ")
 STORAGE_FILE = "storage.json"
 DEFAULT_ENCODING = "utf-8"
+
+
+def add_entry() -> Entry:
+    """
+    Функция для ввода данных и вывода сущности Entry
+    """
+    service = input("Название сервиса - ")
+    login = input("Логин если есть - ")
+    email = input("Email адрес - ")
+    password = input("Пароль - ")
+    return Entry(service, login, email, password)
 
 
 def check_storage(file_name=STORAGE_FILE):
@@ -38,17 +61,19 @@ def read_data(file=STORAGE_FILE):
     return data
 
 
-def write_data(service, email:str, password:str):
+def write_data():
     # TODO: добавить проверку email через регулярное выражение
     # TODO: расширить поиск по сервису, а не только по email
     # NOTE: Добавить Док
-
+    entry = add_entry()
     data = read_data(STORAGE_FILE)
-    new_entry = {"email": email, "password": password}
+    new_entry = asdict(entry)
     data.append(new_entry)
 
     with open(STORAGE_FILE, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-write_data("test", email_input_data, password_input_data)
+write_data()
+
+print(read_data())
