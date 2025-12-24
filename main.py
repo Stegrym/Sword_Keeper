757e9ab072e5e431861ac400a37e567cf61ebda1
Semerikov_Stanislav <stassemerikov@gmail.com>
2025-12-24 17:16:22 +0300

    feat(core): add function delete_entry, update_data - Удаляет запись по параметру service - Заменяет старые данные на новые по параметру service

--- main.py
+++ main.py
@@ -2,32 +2,25 @@ import json
 from pathlib import Path
 from dataclasses import dataclass, asdict
 
+STORAGE_FILE = "storage.json"
+DEFAULT_ENCODING = "utf-8"
+
 
 @dataclass
 class Entry:
+    """
+    Класс для хранения входящих данных
+    """
     service: str
     login: str
     email: str
     password: str
 
 
-# IDEA: добавить GUI через tkinter
-# IDEA: добавить поиск по service/email
-# IDEA: добавить SQLite хранение данных
-
-
-service_data = "service"
-login_data = "login"
-email_data = "email@mail.com"
-password_data = "password1"
-
-STORAGE_FILE = "storage.json"
-DEFAULT_ENCODING = "utf-8"
-
-
 def add_entry() -> Entry:
     """
-    Функция для ввода данных и вывода сущности Entry
+    Функция для ввода данных и вывода объекта Entry
+    :return class Entry
     """
     service = input("Название сервиса - ")
     login = input("Логин если есть - ")
@@ -36,24 +29,17 @@ def add_entry() -> Entry:
     return Entry(service, login, email, password)
 
 
-def check_storage(file_name=STORAGE_FILE):
+def read_data() -> list:
     """
-    :param file_name:
-    :return:
+    Отдаёт список с данными из json STORAGE_FILE
+    :return: список со словарями данных
     """
-    file = Path("storage.json")
-    if not file.exists():
-        file.write_text("[]", encoding=DEFAULT_ENCODING)
 
+    storage_path = Path(STORAGE_FILE)
+    if not storage_path.exists():
+        storage_path.write_text("[]", encoding=DEFAULT_ENCODING)
 
-def read_data(file=STORAGE_FILE):
-    """
-    Open for read JSON file, return json data
-    :param file: json file
-    :return: data:list
-    """
-    check_storage(file)
-    with open(file, "r", encoding="utf-8") as file:
+    with open(STORAGE_FILE, "r", encoding="utf-8") as file:
         try:
             data = json.load(file)
         except json.JSONDecodeError:
@@ -61,37 +47,75 @@ def read_data(file=STORAGE_FILE):
     return data
 
 
-def write_data():
+def write_data() -> bool:
+    """
+    Записывает в хранилище данные, выдаёт статус операции
+    :return: Статус операции по записи данных
+    """
     # TODO: добавить проверку email через регулярное выражение
     # TODO: расширить поиск по сервису, а не только по email
-    # NOTE: Добавить Док
+    write_status = False
     entry = add_entry()
-    data = read_data(STORAGE_FILE)
+    data = read_data()
     new_entry = asdict(entry)
     data.append(new_entry)
 
     with open(STORAGE_FILE, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
+        write_status = True
+    return write_status
 
 
-def search_entry(service: str) -> dict:
+def search_entry(entry: str) -> str:
     """
-    Ищет данные пользователя по сервису
-    :param service:
-    :return: словарь даннных
+    Ищет данные пользователя по вводимому имени сервиса.
+    :param entry:
+    :return: Данные полученные в результате поиска
     """
-    service_info = f"Данные сервиса - {service}: "
+    service_info = f"Данные сервиса - {entry}: "
     storage = read_data()
+    info = ""
+
     for data in storage:
-        if data["service"] == service:
-            info = data
+        if data["service"] == entry:
+            for name, inf in data.items():
+                info += f"\n{name}:{inf}"
             break
+
+    if not info:
         info = "Не найденны"
+
     service_info += info
     return service_info
 
 
-# write_data()
+def delete_entry(entry: str) -> bool:
+    """
+    Ищет и удаляет данные из хранилища
+    :param entry: Данные для удаления
+    :return: статус операции по удалению
+    """
+    data = read_data()
+    new_data = []
+    delete_status = False
+    for item in data:
+        if item["service"] != entry:
+            new_data.append(item)
+        else:
+            delete_status = True
+    with  open(STORAGE_FILE, "w", encoding='utf-8') as file:
+        json.dump(new_data, file, indent=4, ensure_ascii=False)
 
-print(read_data())
-print(search_entry("выф "))
+    return delete_status
+
+
+def update_data(service: str):
+    """
+    Ищет и удаляет данные service и записывает новые
+    :param service:
+    :return:
+    """
+    # TODO Доработать систему обновления данных...
+    delete_status = delete_entry(service)
+    if delete_status:
+        write_data()
